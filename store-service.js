3e18352e7941e881ca4d571bdbf5f1905ad3a091
Rizza Salvador <rsalvador5@myseneca.ca>
2025-02-06 01:15:51 -0500

    update

--- store-service.js
+++ store-service.js
@@ -1,22 +1,23 @@
 const fs = require("fs");
+const path = require("path");
 
 let items = [];
 let categories = [];
 
 const initialize = () => {
     return new Promise((resolve, reject) => {
-        fs.readFile("./data/items.json", "utf8", (err, data) => {
+        fs.readFile(path.join(__dirname, "data", "items.json"), "utf8", (err, data) => {
             if (err) return reject("Unable to read items file");
-            
+
             try {
                 items = JSON.parse(data);
             } catch (error) {
                 return reject("Error parsing items.json");
             }
 
-            fs.readFile("./data/categories.json", "utf8", (err, data) => {
+            fs.readFile(path.join(__dirname, "data", "categories.json"), "utf8", (err, data) => {
                 if (err) return reject("Unable to read categories file");
-                
+
                 try {
                     categories = JSON.parse(data);
                 } catch (error) {
@@ -28,18 +29,3 @@ const initialize = () => {
         });
     });
 };
-
-const getAllItems = () => new Promise((resolve, reject) => {
-    items.length > 0 ? resolve(items) : reject("No results returned");
-});
-
-const getPublishedItems = () => new Promise((resolve, reject) => {
-    const publishedItems = items.filter(item => item.published);
-    publishedItems.length > 0 ? resolve(publishedItems) : reject("No published items available");
-});
-
-const getCategories = () => new Promise((resolve, reject) => {
-    categories.length > 0 ? resolve(categories) : reject("No categories available");
-});
-
-module.exports = { initialize, getAllItems, getPublishedItems, getCategories };
