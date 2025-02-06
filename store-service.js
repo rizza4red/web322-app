bf9ca54961640e683d50747ade72453109170744
Rizza Salvador <rsalvador5@myseneca.ca>
2025-02-06 01:52:03 -0500

    update

--- store-service.js
+++ store-service.js
@@ -5,47 +5,54 @@ let items = [];
 let categories = [];
 
 const initialize = () => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path.join(__dirname, "data", "items.json"), "utf8", (err, data) => {
-            if (err) return reject("Unable to read items file");
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(process.cwd(), "data", "items.json"), "utf8", (err, data) => {
+      if (err) return reject("Unable to read items file");
 
-            try {
-                items = JSON.parse(data);
-            } catch (error) {
-                return reject("Error parsing items.json");
-            }
+      try {
+        items = JSON.parse(data);
+      } catch (error) {
+        return reject("Error parsing items.json");
+      }
 
-            fs.readFile(path.join(__dirname, "data", "categories.json"), "utf8", (err, data) => {
-                if (err) return reject("Unable to read categories file");
+      fs.readFile(path.join(process.cwd(), "data", "categories.json"), "utf8", (err, data) => {
+        if (err) return reject("Unable to read categories file");
 
-                try {
-                    categories = JSON.parse(data);
-                } catch (error) {
-                    return reject("Error parsing categories.json");
-                }
+        try {
+          categories = JSON.parse(data);
+        } catch (error) {
+          return reject("Error parsing categories.json");
+        }
 
-                resolve();
-            });
-        });
+        resolve();
+      });
     });
+  });
 };
 
-const getAllItems = () => new Promise((resolve, reject) => {
+const getAllItems = () =>
+  new Promise((resolve, reject) => {
     items.length > 0 ? resolve(items) : reject("No results returned");
-});
+  });
 
-const getPublishedItems = () => new Promise((resolve, reject) => {
-    const publishedItems = items.filter(item => item.published);
-    publishedItems.length > 0 ? resolve(publishedItems) : reject("No published items available");
-});
+const getPublishedItems = () =>
+  new Promise((resolve, reject) => {
+    const publishedItems = items.filter((item) => item.published);
+    publishedItems.length > 0
+      ? resolve(publishedItems)
+      : reject("No published items available");
+  });
 
-const getCategories = () => new Promise((resolve, reject) => {
-    categories.length > 0 ? resolve(categories) : reject("No categories available");
-});
+const getCategories = () =>
+  new Promise((resolve, reject) => {
+    categories.length > 0
+      ? resolve(categories)
+      : reject("No categories available");
+  });
 
 module.exports = {
-    initialize,
-    getAllItems,
-    getPublishedItems,
-    getCategories
+  initialize,
+  getAllItems,
+  getPublishedItems,
+  getCategories,
 };
